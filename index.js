b5bab6f0826b560c482715defb8e3b92ee4fcafd
Christian Garcia <bones192nd@gmail.com>
2022-12-21 15:42:03 -0500

    commit tree ac3ebd806c60b01969acfbfd46562a9d7ab85ede

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+//test 1
+const test1 = document.getElementById("main");
+test1.remove();
+
+//test 2 + 3
+const newHeader = document.createElement("h1");
+newHeader.id ="victory";
+newHeader.textContent= "Christian is the champion";
+document.body.append(newHeader);
+
+//test 4
+
