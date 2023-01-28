7d8ef4e21df6906ca6813ceda0a0b0d98824c40b
Christian Garcia <bones192nd@gmail.com>
2023-01-27 23:03:12 -0500

    lab submission

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+// test 1
+const main = document.getElementById("main")
+main.remove()
+
+// test 2
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory"
+
+newHeader.textContent = "CHRISTIAN is the champion"
